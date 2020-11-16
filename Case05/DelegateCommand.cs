56d4bb6379b39766045fc7df4cd911b162d3f04a
KAKEI Kazuki <kazuki.kakei.j2f@denso-wave.com>
2020-11-16 17:31:40 +0900

    Case05の追加

---
+++ Case05/DelegateCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Case05
+{
+    internal class DelegateCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public DelegateCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute?.Invoke(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Case05/ViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Case05
+{
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _text;
+        public string Text
+        {
+            set
+            {
+                if (_text != value)
+                {
+                    _text = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Text)));
+                    Debug.WriteLine($"{nameof(Text)} property called. => {value}");
+                    Expression = _text.ToUpper();
+                    ClearCommand.RaiseCanExecuteChanged();
+                }
+            }
+            get
+            {
+                return _text;
+            }
+        }
+
+        private string _expression;
+        public string Expression
+        {
+            set
+            {
+                if (_expression != value)
+                {
+                    _expression = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Expression)));
+                }
+            }
+            get
+            {
+                return _expression;
+            }
+        }
+
+        private DelegateCommand _clearCommand;
+        public DelegateCommand ClearCommand
+        {
+            get
+            {
+                return _clearCommand ?? (_clearCommand = new DelegateCommand(
+                    _ => Text = string.Empty,
+                    _ => !string.IsNullOrEmpty(Text)
+                ));
+            }
+        }
+    }
+}
