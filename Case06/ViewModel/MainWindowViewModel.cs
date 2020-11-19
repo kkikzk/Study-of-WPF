f352e22ac90f3b272a3971e49c39a36657480a22
KAKEI Kazuki <kkikzk@gmail.com>
2020-11-19 16:34:17 +0900

    MainViewにコントロールを追加

--- Case06/ViewModel/MainWindowViewModel.cs
+++ Case06/ViewModel/MainWindowViewModel.cs
@@ -3,5 +3,7 @@
     internal class MainWindowViewModel
     {
         public string WindowTitle { get; } = "割り算計算";
+        public int Height { get; } = 450;
+        public int Width { get; } = 800;
     }
 }
