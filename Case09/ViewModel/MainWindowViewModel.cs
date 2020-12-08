3d43ee298e0ceba478d44bb0b68bff6d5e9060a3
KAKEI Kazuki <kkikzk@gmail.com>
2020-12-08 17:36:28 +0900

    Case09にグリッドを追加

--- Case09/ViewModel/MainWindowViewModel.cs
+++ Case09/ViewModel/MainWindowViewModel.cs
@@ -1,6 +1,33 @@
-﻿namespace Case09.ViewModel
+﻿using System.Collections.ObjectModel;
+
+namespace Case09.ViewModel
 {
     public class MainWindowViewModel
     {
+        public ObservableCollection<GridDataViewModel> GridData
+        {
+            get; set;
+        } = new ObservableCollection<GridDataViewModel>()
+        {
+            new GridDataViewModel() { ID = 1, Name = "Hoge" },
+            new GridDataViewModel() { ID = 2, Name = "Fuga" },
+        };
+    }
+
+    public class GridDataViewModel : NotificationObject
+    {
+        private int _id;
+        public int ID
+        {
+            get { return _id; }
+            set { SetProperty(ref _id, value); }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
     }
 }
